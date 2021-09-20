204fa212629a75e81ffe7575d6fcfd98f2e1690b
Christine Antonio <christinejm.antonio@gmail.com>
2021-09-20 13:37:17 -0700

    Add project files.

---
+++ KuvioSampleProject/Data/EmployeeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KuvioSampleProject.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Sam",
+                LastName = "Antonio",
+                Country = "USA",
+                Birthday = "1992/01/31",
+                Age="29",
+                DateModified = "2021/09/20"
+            },
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                FirstName="Marcelina",
+                LastName="Santiago",
+                Country = "PHL",
+                Birthday ="1985/07/07",
+                Age="36",
+                DateModified="2021/09/20"
+            },
+        };
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+    }
+}
---
+++ KuvioSampleProject/Data/IEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KuvioSampleProject.Data
+{
+    interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+    }
+}
