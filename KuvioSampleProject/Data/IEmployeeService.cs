1c67fb08bfd89ae67c14eebc63d84310c9fbb6f4
Christine Antonio <christinejm.antonio@gmail.com>
2021-09-21 08:47:06 -0700

    GET all, GET by Id, Add, Edit, Delete functionality

--- KuvioSampleProject/Data/IEmployeeService.cs
+++ KuvioSampleProject/Data/IEmployeeService.cs
@@ -8,5 +8,13 @@ namespace KuvioSampleProject.Data
     interface IEmployeeService
     {
         List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void UpdateEmployee(Employee employee);
+
+        void AddEmployee(Employee employee);
+
+        void DeleteEmployee(Guid id);
     }
 }
