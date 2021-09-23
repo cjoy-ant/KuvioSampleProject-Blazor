97d3b29272aa203268021e3d168bd61a66257110
Christine Antonio <christinejm.antonio@gmail.com>
2021-09-23 14:55:57 -0700

    CRUD methods for /employees

---
+++ KuvioSampleProject.Api/Models/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using KuvioSampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KuvioSampleProject.Api.Models
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(Guid id);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        void DeleteEmployee(Guid id);
+
+    }
+}
